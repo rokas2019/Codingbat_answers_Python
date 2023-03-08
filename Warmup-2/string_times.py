cae18da49dc8e6aecd73c0b26647e7a1a1fd9494
Rokas Galdikas <r.galdikas911@gmail.com>
2023-03-08 14:22:05 +0200

    Warmup-2 answers

---
+++ Warmup-2/string_times.py
@@ -0,0 +1,10 @@
+# Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+#
+#
+# string_times('Hi', 2) → 'HiHi'
+# string_times('Hi', 3) → 'HiHiHi'
+# string_times('Hi', 1) → 'Hi'
+
+def string_times(str, n):
+    if n >= 0:
+        return str * n
